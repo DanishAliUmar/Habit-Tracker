199524fcf98bc0f2c1210771f961b3f5dd7d44ae
DanishAliUmar <danishaliumar4@gmail.com>
2024-09-30 02:46:24 +0500

    Working on redux setup ..

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-function App() {
+import { Provider } from "react-redux";
+import store from "./store/store";
 
+function App() {
   return (
-   <div className="">
-    123456789
-   </div>
-  )
+    <Provider store={store}>
+      <div className="">123456789</div>
+    </Provider>
+  );
 }
 
-export default App
+export default App;
