a462f1473f3b141cd15d739b7318d23092e427b6
DanishAliUmar <danishaliumar4@gmail.com>
2024-10-07 23:57:55 +0500

    working on UI ..

--- src/components/add-habit-form.tsx
+++ src/components/add-habit-form.tsx
@@ -31,36 +31,36 @@ const AddHabitForm: React.FC = () => {
     }
   };
   return (
-    // <form onSubmit={handleSubmit}>
-    //   <Box
-    //     sx={{
-    //       display: "flex",
-    //       flexDirection: "column",
-    //       gap: 2,
-    //     }}
-    //   >
-    //     <TextField
-    //       label="Habit Name"
-    //       value={name}
-    //       onChange={(e) => setName(e.target.value)}
-    //       placeholder="Enter habit name"
-    //       fullWidth
-    //     />
-    //     <FormControl fullWidth>
-    //       <InputLabel>Frequency</InputLabel>
-    //       <Select
-    //         value={frequency}
-    //         onChange={(e) => setFrequency(e.target.value as "daily" | "weekly")}
-    //       >
-    //         <MenuItem value={"daily"}>Daily</MenuItem>
-    //         <MenuItem value={"weekly"}>Weekly</MenuItem>
-    //       </Select>
-    //     </FormControl>
-    //     <Button type="submit" variant="contained" color="primary">
-    //       Add Habit
-    //     </Button>
-    //   </Box>
-    // </form>
+    <form onSubmit={handleSubmit}>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          gap: 2,
+        }}
+      >
+        <TextField
+          label="Habit Name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          placeholder="Enter habit name"
+          fullWidth
+        />
+        <FormControl fullWidth>
+          <InputLabel>Frequency</InputLabel>
+          <Select
+            value={frequency}
+            onChange={(e) => setFrequency(e.target.value as "daily" | "weekly")}
+          >
+            <MenuItem value={"daily"}>Daily</MenuItem>
+            <MenuItem value={"weekly"}>Weekly</MenuItem>
+          </Select>
+        </FormControl>
+        <Button type="submit" variant="contained" color="primary">
+          Add Habit
+        </Button>
+      </Box>
+    </form>
   );
 };
 
