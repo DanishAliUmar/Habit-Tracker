a2b7b7cefe1c4b2f8092908c3ae0b5a6a8cc4071
DanishAliUmar <danishaliumar4@gmail.com>
2024-10-13 23:42:49 +0500

    print habits .....

--- src/components/habit-list.tsx
+++ src/components/habit-list.tsx
@@ -9,7 +9,14 @@ const HabitList: React.FC = () => {
   return (
     <Box sx={{ display: "flex", flexDirection: "column", gap: 2, mt: 4 }}>
       {habits.map((habit)=>{
-        return <div className=""></div>
+        return <Paper key={habit.id} elevation={2} sx={{p:2}}>
+          <Grid container alignItems={"center"}>
+            <Grid>
+              <Typography variant="h6">{habit.name}</Typography>
+              <Typography variant="body2" color="textSecondary">{habit.frequency}</Typography>
+            </Grid>
+          </Grid>
+        </Paper>
       })}
     </Box>
   );
