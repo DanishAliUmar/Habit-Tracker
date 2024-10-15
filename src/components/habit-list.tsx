c2b966ce56aba24bf11c347b36a7037ef177629d
DanishAliUmar <danishaliumar4@gmail.com>
2024-10-15 05:10:27 +0500

    Working on UI ...

--- src/components/habit-list.tsx
+++ src/components/habit-list.tsx
@@ -1,22 +1,60 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import { RootState } from "../store/store";
-import { Box, Grid, Paper, Typography } from "@mui/material";
+import { Box, Button, Grid, Paper, Typography } from "@mui/material";
+import { CheckCircleOutline, Delete } from "@mui/icons-material";
 
 const HabitList: React.FC = () => {
   const habits = useSelector((state: RootState) => state.habits.habits);
 
+  const today = new Date().toISOString().split("T")[0];
   return (
     <Box sx={{ display: "flex", flexDirection: "column", gap: 2, mt: 4 }}>
-      {habits.map((habit)=>{
-        return <Paper key={habit.id} elevation={2} sx={{p:2}}>
-          <Grid container alignItems={"center"}>
-            <Grid>
-              <Typography variant="h6">{habit.name}</Typography>
-              <Typography variant="body2" color="textSecondary">{habit.frequency}</Typography>
+      {habits.map((habit) => {
+        return (
+          <Paper key={habit.id} elevation={2} sx={{ p: 2 }}>
+            <Grid container alignItems={"center"}>
+              <Grid>
+                <Typography variant="h6">{habit.name}</Typography>
+                <Typography
+                  variant="body2"
+                  color="textSecondary"
+                  textTransform={"capitalize"}
+                >
+                  {habit.frequency}
+                </Typography>
+              </Grid>
+              <Grid xs={12} sm={6}>
+                <Box
+                  sx={{ display: "flex", justifyContent: "flex-end", gap: 1 }}
+                >
+                  <Button
+                    variant="outlined"
+                    color={
+                      habit.completedDates.includes(today)
+                        ? "success"
+                        : "primary"
+                    }
+                    startIcon={<CheckCircleOutline />}
+                  >
+                    {
+                      habit.completedDates.includes(today)
+                        ? "Completed"
+                        : "Mark Complete"
+                    }
+                  </Button>
+                  <Button
+                    variant="outlined"
+                    color={"error"}
+                    startIcon={<Delete />}
+                  >
+                    Mark Complete
+                  </Button>
+                </Box>
+              </Grid>
             </Grid>
-          </Grid>
-        </Paper>
+          </Paper>
+        );
       })}
     </Box>
   );
