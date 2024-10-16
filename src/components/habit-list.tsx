2bc95f328b80d9be95bd6ef6ae5bbe499da82624
DanishAliUmar <danishaliumar4@gmail.com>
2024-10-16 05:32:30 +0500

    complete habit Tracker with dummy api ..

--- src/components/habit-list.tsx
+++ src/components/habit-list.tsx
@@ -1,14 +1,38 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { AppDispatch, RootState } from "../store/store";
-import { Box, Button, Grid, Paper, Typography } from "@mui/material";
+import {
+  Box,
+  Button,
+  Grid,
+  LinearProgress,
+  Paper,
+  Typography,
+} from "@mui/material";
 import { CheckCircleOutline, Delete } from "@mui/icons-material";
-import { toggleHabit } from "../store/habit-slice";
+import { Habit, toggleHabit } from "../store/habit-slice";
 
 const HabitList: React.FC = () => {
   const habits = useSelector((state: RootState) => state.habits.habits);
   const dispatch = useDispatch<AppDispatch>();
   const today = new Date().toISOString().split("T")[0];
+
+  const getStreak = (habit: Habit) => {
+    let streak = 0;
+    const currentDate = new Date();
+
+    while (true) {
+      const dateString = currentDate.toISOString().split("T")[0];
+      if (habit.completedDates.includes(dateString)) {
+        streak++;
+        currentDate.setDate(currentDate.getDate() - 1);
+      } else {
+        break;
+      }
+    }
+
+    return streak;
+  };
   return (
     <Box sx={{ display: "flex", flexDirection: "column", gap: 2, mt: 4 }}>
       {habits.map((habit) => {
@@ -55,6 +79,16 @@ const HabitList: React.FC = () => {
                 </Box>
               </Grid>
             </Grid>
+            <Box sx={{ mt: 2 }}>
+              <Typography variant="body2">
+                Current Streak : {getStreak(habit)} days
+              </Typography>
+              <LinearProgress
+                variant="determinate"
+                value={(getStreak(habit) / 30) * 100}
+                sx={{ mt: 1 }}
+              />
+            </Box>
           </Paper>
         );
       })}
